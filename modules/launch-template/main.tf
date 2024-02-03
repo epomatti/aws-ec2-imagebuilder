0d7eab761ca17161be1f60d706f844502385132f
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-02-03 15:27:29 -0300

    egress ports

--- modules/launch-template/main.tf
+++ modules/launch-template/main.tf
@@ -92,19 +92,9 @@ resource "aws_security_group" "elb" {
 resource "aws_security_group_rule" "outbound_http" {
   description       = "HTTP Outbound"
   type              = "egress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.elb.id
-}
-
-resource "aws_security_group_rule" "outbound_https" {
-  description       = "HTTPS Outbound"
-  type              = "egress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.elb.id
 }
